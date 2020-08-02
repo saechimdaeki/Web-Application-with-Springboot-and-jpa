604ff0829c802ab4f1773720235a9500a8621993
김준성 <anima94@naver.com>
2020-08-02 17:08:09 +0900

    주문상품 엔티티 dev :orange_book:

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.domain.item.DeliveryStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -68,6 +69,47 @@ public class Order {
     public void setDelivery(Delivery delivery){
         this.delivery=delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메소드==//
+    public static Order createOrder(Member member,Delivery delivery, OrderItem... orderItems){
+        Order order=new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem:orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if(delivery.getStatus()== DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem: orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //== 조회 로직 ==//
+
+    /**
+     * 전체 주문 가격조회
+     */
+    public int getTotalPrice(){
+       int totalPrice=0;
+       for(OrderItem orderItem:orderItems){
+           totalPrice+=orderItem.getTotalPrice();
+       }
+       return totalPrice;
+       //       return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum(); 위방식의 한줄코드
     }
 
 }
