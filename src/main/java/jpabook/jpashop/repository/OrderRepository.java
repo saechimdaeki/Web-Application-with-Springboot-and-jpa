023c2edf75f97ce8402aa12de62871c2e1e009e2
김준성 <anima94@naver.com>
2020-08-03 11:57:21 +0900

    jpa 동적처리방법 :mount_fuji:

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,9 +2,15 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.hibernate.Criteria;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,7 +27,93 @@ public class OrderRepository {
         return em.find(Order.class,id);
     }
 
-   /// public List<Order> findAll(OrderSearch orderSearch){
+    //jpa 동적으로 만들기
 
+    /**
+     * 1번째방법 String 으로 하는방법
+     * 안쓰는게 좋은방법.
+     */
+    public List<Order> findAllByString(OrderSearch orderSearch){
+        String jpql="select o from Order o join o.member m";
+        boolean isFirstCondition=true;
 
+        //주문 상태 검색
+        if(orderSearch.getOrderStatus()!=null){
+            if(isFirstCondition){
+                jpql+=" where";
+                isFirstCondition=false;
+            }else{
+                jpql+=" and";
+            }
+            jpql+=" o.status = :status";
+        }
+
+        //회원 이름 검색
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+            if(isFirstCondition){
+                jpql+=" where";
+                isFirstCondition=false;
+            }else{
+                jpql += " and";
+            }
+            jpql+=" m.name like :name";
+        }
+
+        TypedQuery<Order> query=em.createQuery(jpql,Order.class)
+                .setMaxResults(1000);
+
+        if(orderSearch.getOrderStatus()!=null){
+            query=query.setParameter("status",orderSearch.getOrderStatus());
+        }
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+            query=query.setParameter("name",orderSearch.getMemberName());
+        }
+        /* JPQL을 문자로 생성하는것은 정말번거롭고 실수가잦을수있다..*/
+       return query.getResultList();
+    }
+
+     /**
+     * 2번째 방법 JPA Criteria 방법
+     * 참고: 김영한님 피셜로 이건 사람이 쓰라고 만든방법이아니라고함..(실무에서 유지보수 개어려움)
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch){
+        CriteriaBuilder cb=em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq=cb.createQuery(Order.class);
+        Root<Order> o=cq.from(Order.class);
+        Join<Object,Object> m=o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria=new ArrayList<>();
+
+        //주문 상태 검색
+        if(orderSearch.getOrderStatus()!=null){
+            Predicate status=cb.equal(o.get("status"),orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        //회원 이름 검색
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+            Predicate name=cb.like(m.<String>get("name"),"%"+orderSearch.getMemberName()+"%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query=em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
+    /**
+     * 3번째 방법 Querydsl 방법
+     * 이방법이 실무에서 조건에 따라 실행되는 쿼리가달라지는 동적쿼리에서 많이사용되는방법
+     */
+    /*
+    public List<Order> findAll(OrderSearch orderSearch){
+        QOrder order=Qorder.order;
+        QMember member=QMember.member;
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member,member)
+                .where(statusEq(orderSearch.getOrderStatus()),
+                        nameLike(orderSearch.getMemberName()))
+                .Limit(1000)
+                .fetch();
+    }
+    */
 }
