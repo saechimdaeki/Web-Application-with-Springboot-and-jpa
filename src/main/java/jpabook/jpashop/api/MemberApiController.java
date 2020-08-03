ca4c6d272444d9d44b19b257381a6b92977c334f
김준성 <anima94@naver.com>
2020-08-03 22:13:54 +0900

    회원 등록 api :

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+//@Controller + @ResponseBody =@RestController
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        Long id=memberService.join(member);
+        return new CreateMemberResponse(id);
+        /* 실무에서 api를만들때는 항상 entity를 파라미터로 받지말자. 그리고 entity를 웹에 노출해서도 안된다.*/
+    }
+
+    /* 아래의 v2같은 방식을 쓰자. */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member=new Member();
+        member.setName(request.getName());
+        Long id=memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+    @Data
+    static class CreateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
